8efaa801a30c3572529c12be1826af17ce1ba1e8
Tiago Alves <tiago.alvessilva67@gmail.com>
2023-10-09 13:42:24 -0300

    Inserindo novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmalCardComponent } from './components/smal-card/smal-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmalCardComponent
   ],
   imports: [
     BrowserModule,
