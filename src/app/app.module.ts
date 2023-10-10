eee0129b45e928f071ac3c58bd1552d8ed4fa37f
Tiago Alves <tiago.alvessilva67@gmail.com>
2023-10-09 23:01:07 -0300

    alterando o tiulo e big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmalCardComponent } from './components/smal-card/smal-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SmalCardComponent } from './components/smal-card/smal-card.component';
     MenuBarComponent,
     MenuTitleComponent,
     BigCardComponent,
-    SmalCardComponent
+    SmalCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
