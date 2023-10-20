02030112077a828a06f50d63ad1d391de9515c65
Tiago Alves <tiago.alvessilva67@gmail.com>
2023-10-20 14:12:11 -0300

    Atualizações

--- src/app/components/smal-card/smal-card.component.ts
+++ src/app/components/smal-card/smal-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-smal-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./smal-card.component.css']
 })
 export class SmalCardComponent {
-
+  @Input()
+  photoCover:string = ""
+  @Input()
+  CardTitle:string = ""
 }
